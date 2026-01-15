db6123149d49540d22376e1c98bec11701ddd5f4
alinconstantin1 <alinwillwide@gmail.com>
2026-01-15 16:50:35 +0200

    dt dd

---
+++ src/settings/DtDd.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    const {
+        dtText,
+        dtTextEnd,
+        children,
+        afterDd,
+        open: defaultOpen = false,
+    }: {
+        dtText: string;
+        dtTextEnd?: Snippet;
+        children: Snippet;
+        afterDd?: Snippet;
+        open?: boolean;
+    } = $props();
+
+    let open = $derived.by(() => {
+        const storedToggle = localStorage.getItem(`${dtText} Open`);
+        if (storedToggle === null) return defaultOpen;
+        return storedToggle === "true";
+    });
+
+    $effect(() => {
+        localStorage.setItem(`${dtText} Open`, open.toString());
+    });
+</script>
+
+<dt>
+    <label>
+        <input type="checkbox" bind:checked={open} /><span
+        ></span>{dtText}{@render dtTextEnd?.()}{#if open}:{/if}
+    </label>
+</dt>
+{#if open}
+    <dd>
+        {@render children()}
+    </dd>
+    {@render afterDd?.()}
+{/if}
+
+<style>
+    label {
+        cursor: pointer;
+    }
+    input[type="checkbox"] {
+        display: none;
+        & + span::after {
+            content: "▸";
+        }
+        &:checked + span::after {
+            content: "▾";
+        }
+    }
+</style>
