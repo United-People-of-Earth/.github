7e4e6a798e26f94d82164787a597090f9a95ddf6
alinconstantin1 <alinwillwide@gmail.com>
2026-01-15 16:49:18 +0200

    settings init

---
+++ src/settings/Settings.svelte
@@ -0,0 +1,20 @@
+<script>
+    const defaultOpen = true;
+
+    let open = $derived.by(() => {
+        const storedOpen = localStorage.getItem("Settings Open");
+        if (storedOpen === null) return defaultOpen;
+        return storedOpen === "true";
+    });
+
+    $effect(() => {
+        localStorage.setItem("Settings Open", open.toString());
+    });
+</script>
+
+<details bind:open>
+    <summary>Settings</summary>
+    <dl>
+        
+    </dl>
+</details>
